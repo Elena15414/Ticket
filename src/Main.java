a9216973953b2c994fb55806452331215e05e879
Elena Datsenko <Elenasheglova15414@Gmail.com>
2023-02-17 01:52:17 +0300

    read

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-
-        System.out.println();
-        System.out.println(service.calculate(10000)); // должно получиться 500
-
-        System.out.println();
-        System.out.println(service.calculate(20000)); // должно получиться 1000
-
-        System.out.println();
-        System.out.println(service.calculate(25000)); // должно получиться 1250
+        int price = 25_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
+
+
+
+ //Консоль
+// "C:\Program Files\Eclipse Adoptium\jdk-11.0.17.8-hotspot\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\lib\idea_rt.jar=65204:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\Татьяна\ticket\out\production\ticket Main
+//1250
+//
+//Process finished with exit code 0
